0b8488b2793bd272789a6a042fecf5a5138f98e8
misslild <daniela.fedyakin@gmail.com>
2017-11-14 22:44:38 +0100

    Rearranged files

---
+++ 1 - Drum Kit/main.js
@@ -0,0 +1,21 @@
+function playSound(event) {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  if(!audio) return; //stops the function
+  audio.currentTime=0; //rewind to the start
+  audio.play();
+  key.classList.add('playing');
+}
+
+
+function removeTransition(event) {
+  if(event.propertyName !== 'transform') {
+    return;
+  }
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+window.addEventListener('keydown',playSound);
