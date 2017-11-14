b49b23aaf76667ede6ee27386855f18063b6aea9
misslild <daniela.fedyakin@gmail.com>
2017-11-14 22:24:30 +0100

    10th project added

---
+++ #10 Use Shift for multiple selection/main.js
@@ -0,0 +1,22 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+let lastChecked;
+
+function handleCheck(event) {
+  let inBetween = false;  
+  if (event.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    })
+  }
+
+  lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => 
+  checkbox.addEventListener('click', handleCheck));
