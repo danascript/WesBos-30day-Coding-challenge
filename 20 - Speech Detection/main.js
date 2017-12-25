e227c6b390df08c4b9b1f564e5e47f67aa5e04af
misslild <daniela.fedyakin@gmail.com>
2017-12-25 19:34:22 +0100

    20th project added

---
+++ 20 - Speech Detection/main.js
@@ -0,0 +1,30 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition()
+recognition.interimResults = true
+
+let p = document.createElement('p')
+const words = document.querySelector('.words')
+words.appendChild(p)
+
+recognition.addEventListener('result', e => {
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('')
+
+    p.textContent = transcript
+
+    if (e.results[0].isFinal) {
+      p = document.createElement('p')
+      words.appendChild(p)
+    }
+
+    // if (transcript.includes('sunny')) {
+    //   console.log('Yesterday my life was filled with rain')
+    // }
+  })
+
+recognition.addEventListener('end', recognition.start)
+
+recognition.start()
