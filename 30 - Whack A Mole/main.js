959e81fc5348f3b72dca8795ed5eccb345b82e77
misslild <daniela.fedyakin@gmail.com>
2017-12-26 21:00:05 +0100

    Updated main.js
    Fixed a typo and added highscore count

--- 30 - Whack A Mole/main.js
+++ 30 - Whack A Mole/main.js
@@ -1,54 +1,70 @@
-const holes = document.querySelectorAll('.hole')
-const scoreBoard = document.querySelector('.score')
-const moles = document.querySelectorAll('.mole')
-let lastHole
-let timeUp = false
-let score = 0
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+const startButton = document.querySelector('button');
+const highscoreBoard = document.querySelector('.highscore')
+
+
+let lastHole;
+let timeUp = false;
+let score = 0;
+let highscore = 0
+highscoreBoard.textContent = highscore
 
 function randomTime(min, max) {
-  return Math.round(Math.random() * (max - min) + min)
+  return Math.round(Math.random() * (max - min) + min);
 }
 
 function randomHole(holes) {
-  const idx = Math.floor(Math.random() * holes.length)
-  const hole = holes[idx]
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+
   if (hole === lastHole) {
-    return randomHole(holes)
+    return randomHole(holes);
   }
 
-  lastHole = hole
-  return hole
+  lastHole = hole;
+  return hole;
 }
 
 function peep() {
-  const time = randomTime(200, 1000)
-  const hole = randomHole(holes)
-  hole.classList.add('up')
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
 
+  hole.classList.add('up');
   setTimeout(() => {
-    hole.classList.remove('up')
-    if(!timeUp) {
-      peep()
-    }
-  }, time)
+    hole.classList.remove('up');
+    if(!timeUp) peep();
+  }, time);
 }
 
 function startGame() {
-  scoreBoard.textContent = 0
-  timeup = false
-  score = 0
-  peep()
-  setTimeout(() => timeUp = true, 2000)
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+
+  setTimeout(() => timeUp = true, 5000);
 }
 
 function bonk(e) {
   if (!e.isTrusted) {
-    return
+    return;
   }
 
-  score++
-  this.classList.remove('up')
-  scoreBoard.textContent = score
+  score++;
+
+  if (score >= highscore) {
+      highscore = score;
+    }
+
+  this.parentNode.classList.remove('up');
+
+  scoreBoard.textContent = score;
+  highscoreBoard.textContent = highscore;
 }
 
-moles.forEach(mole => mole.addEventListener('click', bonk))
+moles.forEach(mole => mole.addEventListener('click', bonk));
+startButton.addEventListener("click", startGame);
+
+  
